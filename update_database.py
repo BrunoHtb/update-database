30dec4ec539c49a0272764c18abd58134a33ef2b
Bruno Taborda <bruno.taborda1711@gmail.com>
2024-02-07 11:06:49 -0300

    Add gitignore

--- update_database.py
+++ update_database.py
@@ -1,17 +1,23 @@
 import psycopg2
 import schedule
 import time
-
-connection_config = psycopg2.connect(
-    host="177.220.159.198",
-    database="Esteio",
-    user="postgres",
-    password="cadastro"
-)
+from datetime import datetime
+from decouple import config
 
 def update_database():
     file_name = r"C:\Users\0519\Documents\Padronizacao_Banco_Dados\updateGeral.sql"
     commands = []
+    db_host = config('DB_HOST', default='localhost')
+    db_name = config('DB_NAME', default='Teste')
+    db_user = config('DB_USER', default='postgres')
+    db_password = config('DB_PASSWORD', default='teste')
+
+    connection_config = psycopg2.connect(
+        host=db_host,
+        database=db_name,
+        user=db_user,
+        password=db_password
+    )
 
     connection = connection_config.cursor()
 
@@ -19,18 +25,26 @@ def update_database():
         content = file.read()
 
     commands = content.split(';')
+    start_date = datetime.now().strftime("%d de %B de %Y, %H:%M")
+
     for command in commands:
         try:
             print(command)
             connection.execute(command)
             connection_config.commit()
         except Exception as e:
-            print(f"Erro ao executar o comando: {command}\nErro: {e}")
+            print(f"Erro ao executar o comando: {command}\n Erro: {e}")
             connection_config.rollback()
     connection.close()
     connection_config.close() 
 
-schedule.every(3).hours.do(update_database)
+    end_date = datetime.now().strftime("%d de %B de %Y, %H:%M")
+    print("Começou as: ", start_date)
+    print("Terminou as:", end_date)
+
+update_database()
+schedule.every(2).hours.do(update_database)
 while True:
+    print("Aguardando...")
     schedule.run_pending()
     time.sleep(3600)
